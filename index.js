359bbf9f10146e509fce49da581be5d3dd0cddfe
unknown <osama.albader99@gmail.com>
2024-10-08 22:28:23 +0300

    variables task

--- index.js
+++ index.js
@@ -1,58 +1,28 @@
-/**
- * Part 1: Creating Variables and Constants
- * In this file you should define the following variables with the exact names
- *
- * 1. fullName      -> assign it a value of your full name
- * 2. yearOfBirth   -> assign it a value of your year of birth
- * 3. hobby         -> assign it a value of your favorite hobby
- * 4. funFact       -> assign it a value of some fun fact about yourself
- * 5. image         -> assign it a value of a url of your image or ant image that represents you online
- */
+let fullName = "osama albader";
+let yearOfBirth = 2000;
+let hobby = "playing football";
+let funFact = "i sleep a lot";
+const image =
+  "https://plus.unsplash.com/premium_photo-1682096259050-361e2989706d?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8eW91bmclMjBtYW58ZW58MHx8MHx8fDA%3D";
 
-// Part 1 answer 👇🏻 ...
-
-
-
-
-
-
-/**
- * Part 2: String Interpolation
- * Create the following new variables that interpolate
- * the variables defined above into strings.
- *
- * 1. fullNameString      -> assign it to: My name is {fullName}
- * 2. yearOfBirthString   -> assign it to: I am {YOUR_AGE}, and make sure you calculate your age from your year of birth
- * 3. hobbyString         -> assign it to: My hobby is {YOUR_HOBBY}
- * 
- * Uncomment the following lines and start interpolating.
- * You can uncomment a line by removing the // at the start
- */
-
-// const fullNameString = `My name is ${}`;
-// const yearOfBirthString =
-// const hobbyString =
-
-
-
-
-/**
- * Part 3: Re-assignment
- * Increment your hacker score
- * */
+let currentYear = new Date().getFullYear();
+let age = currentYear - yearOfBirth;
+const fullNameString = `My name is ${fullName}`;
+const yearOfBirthString = `i am  ${age}`;
+const hobbyString = `i like to do ${hobby}`;
 
 let stars = 0;
 
 function incrementBy1() {
-  // Increment stars by 1 👇🏻
+  stars = stars + 1;
 }
 function decrementBy1() {
-  // decrement stars by 1 👇🏻
+  stars = stars - 1;
 }
 
 function incrementBy2() {
-  // Increment stars by 2 👇🏻
+  stars = stars + 2;
 }
 function decrementBy2() {
-  // decrement stars by 2 👇🏻
+  stars = stars - 2;
 }
